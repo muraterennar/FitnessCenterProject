062fc415785891bd63265276c8d8ab8de296ce14
Murat Eren Nar <muraterennar@hotmail.com>
2021-12-06 15:35:10 +0300

    DataAccess Concreate fillin

---
+++ Entities/Concreate/Membership.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concreate
+{
+    public class Membership : IEntity
+    {
+        public int Id { get; set; }
+        public int SubscriptionId { get; set; }
+    }
+}
